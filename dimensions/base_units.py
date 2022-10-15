76ca4b7e57bacfeb5ddc6ad6d5d2feb752a9633c
Jacob Lee <JLpython@outlook.com>
2022-10-15 14:55:36 -0700

    feat: create dimensions.base_units.BaseUnit class

---
+++ dimensions/base_units.py
@@ -0,0 +1,46 @@
+"""
+
+"""
+
+
+class BaseUnit:
+    """
+
+    """
+    def __init__(self, name: str, abbr: str):
+        self._name = name
+        self._abbr = abbr
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(name={self.name}, abbr={self.abbr})"
+
+    def __str__(self) -> str:
+        return self.abbr
+
+    def __eq__(self, other) -> bool:
+        """
+
+        :param other:
+        :type other: BaseUnit
+        :return:
+        """
+        if not issubclass(type(other), BaseUnit):
+            raise TypeError
+
+        return type(self) == type(other)
+
+    @property
+    def abbr(self) -> str:
+        """
+
+        :return:
+        """
+        return self._abbr
+
+    @property
+    def name(self) -> str:
+        """
+
+        :return:
+        """
+        return self._name
